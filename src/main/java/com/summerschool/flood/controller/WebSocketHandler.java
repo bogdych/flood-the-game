b07fe7b49db501b7f810d65c6c0f9cb5eab52193
Dmitrii Khudyshkin <Dmitrii.Khudyshkin@lanit-tercom.com>
2019-07-04 16:20:19 +0300

    + example of websocket without socketJs

---
+++ src/main/java/com/summerschool/flood/controller/WebSocketHandler.java
@@ -0,0 +1,30 @@
+package com.summerschool.flood.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    private List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        for (WebSocketSession webSocketSession : sessions) {
+            webSocketSession.sendMessage(message);
+        }
+        super.handleTextMessage(session, message);
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        //the messages will be broadcasted to all users.
+        sessions.add(session);
+    }
+}
