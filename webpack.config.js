aef600057672b06f9881c42e4639e3fccfb99d5f
Unknown <bogdyich@gmail.com>
2019-07-13 20:26:32 +0300

    Add assets copying

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -8,6 +9,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin(),
+    new CopyPlugin([
+      { from: './src/assets', to: './assets' },
+    ])
   ]
 };
