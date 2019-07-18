048928bb15911ae0c402a96c664787a190c536a1
TimurShangareev <cho.tebe@mail.ru>
2019-07-18 19:16:03 +0300

    Some corrections after tests

--- src/main/java/com/summerschool/flood/field/DepthFirstSearch.java
+++ src/main/java/com/summerschool/flood/field/DepthFirstSearch.java
@@ -9,21 +9,25 @@ public class DepthFirstSearch {
     /* O(|V| + |E|) */
     public void start(Pair<Integer, Integer> cell, int value){
         startValue = (int) matrix.getAt(cell.getFirst(), cell.getSecond());
-        run(cell.getFirst(), cell.getSecond(), value);
+        runNext(cell.getFirst(), cell.getSecond(), value);
     }
     public void start(int xCell, int yCell, int value){
         startValue = (int) matrix.getAt(xCell, yCell);
-        run(xCell, yCell, value);
+        runNext(xCell, yCell, value);
     }
-    private void run(int xCell, int yCell, int value){
+    private void runNext(int xCell, int yCell, int value){
         /*"used" is excess. Think, that startValue is enough*/
         if(!matrix.isInternalCell(xCell, yCell) || matrix.getAt(xCell, yCell) != startValue)
             return;
         matrix.setAt(xCell, yCell, value);
 
-        run(xCell - 1, yCell, value);
-        run(xCell, yCell + 1, value);
-        run(xCell + 1, yCell, value);
-        run(xCell, yCell - 1, value);
+        if(matrix.isInternalCell(xCell - 1, yCell) && matrix.getAt(xCell - 1, yCell) == startValue)
+            runNext(xCell - 1, yCell, value);
+        if(matrix.isInternalCell(xCell, yCell + 1) && matrix.getAt(xCell, yCell + 1) == startValue)
+            runNext(xCell, yCell + 1, value);
+        if(matrix.isInternalCell(xCell + 1, yCell) && matrix.getAt(xCell + 1, yCell) == startValue)
+            runNext(xCell + 1, yCell, value);
+        if(matrix.isInternalCell(xCell, yCell - 1) && matrix.getAt(xCell, yCell - 1) == startValue)
+            runNext(xCell, yCell - 1, value);
     }
 }
