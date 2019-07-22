df3160774550557e5c2296676e5876169fb82cc7
TimurShangareev <cho.tebe@mail.ru>
2019-07-22 14:57:58 +0300

    DFS and makeStep replaced out from Field class. Added switch part in makeAction.

--- src/main/java/com/summerschool/flood/game/FloodGame.java
+++ src/main/java/com/summerschool/flood/game/FloodGame.java
@@ -1,21 +1,28 @@
 package com.summerschool.flood.game;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import static com.summerschool.flood.game.GameStatus.FINISHED;
 import static com.summerschool.flood.game.GameStatus.READY;
 
-@Data
 public class FloodGame implements IGame {
 
-    private List<Player> players = new ArrayList<>();
-    private long id;
-    private Field field;
-    private int maxPlayers;
-    private GameStatus gameStatus;
+    private @Getter @Setter List<Player> players = new ArrayList<>();
+    private @Getter Field field;
+    private @Getter @Setter long id;
+    private @Getter Map<Player, Cell> playersStartPosition = new ConcurrentHashMap<>();
+    private @Getter int counter = 0;
+    private IFirstSearch firstSearch;
+    private @Getter @Setter int maxPlayers;
+    private @Getter @Setter GameStatus gameStatus;
 
     public FloodGame(GameType type, int maxPlayersCount) {
         this.maxPlayers = maxPlayersCount;
@@ -28,6 +35,7 @@ public class FloodGame implements IGame {
             case FAST:
                 this.field = new Field(5, 5);
         }
+        this.firstSearch = new DepthFirstSearch(field);
     }
 
     @Override
@@ -61,8 +69,41 @@ public class FloodGame implements IGame {
 
     @Override
     public Result makeAction(Player player, GameAction action) {
-        // todo: run game logic here
+        switch (action.getType()){
+            case MAKE_STEP:
+                if(action.getParams().get("X1") instanceof Integer &&
+                        action.getParams().get("X2") instanceof Integer &&
+                        action.getParams().get("color") instanceof  Color) {
+
+                    if(isValidMakeStep(player, (Integer)action.getParams().get("X1"),
+                            (Integer) action.getParams().get("X1"),
+                            (Color) action.getParams().get("Color"))) {
+
+                        makeStep(player, (Integer) action.getParams().get("X1"),
+                                (Integer) action.getParams().get("X2"),
+                                (Color) action.getParams().get("Color"));
+                    }
+                }
+
+                break;
+            default:
+                return null;
+        }
         return null;
+    }
+
+    public void makeStep(int x, int y, Color color){
+        Cell tmpCell = new Cell(x, y);
+        tmpCell.setColor(color);
+        firstSearch.start(tmpCell);
+        counter = (counter + 1) % playersStartPosition.size();
+    }
+    public Boolean isValidMakeStep(Player player, int x, int y, Color color){
+        Cell tmpCell = playersStartPosition.get(counter);
+        return player == players.get(counter) &&
+                tmpCell.getX() == x && tmpCell.getY() == y &&
+                tmpCell.getColor() != color &&
+                field.isInternalAt(x, y);
     }
 }
 
---
+++ src/main/java/com/summerschool/flood/game/IFirstSearch.java
@@ -0,0 +1,5 @@
+package com.summerschool.flood.game;
+
+public interface IFirstSearch {
+    void start(Cell cell);
+}
