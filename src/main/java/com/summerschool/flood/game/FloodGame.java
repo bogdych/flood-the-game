5a3bf368866ed337df8a46973fe73ee6050a8e68
TimurShangareev <cho.tebe@mail.ru>
2019-07-22 14:57:58 +0300

    DFS and makeStep replaced out from Field class. Added switch part in makeAction.

--- src/main/java/com/summerschool/flood/game/FloodGame.java
+++ src/main/java/com/summerschool/flood/game/FloodGame.java
@@ -2,6 +2,7 @@ package com.summerschool.flood.game;
 
 import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 import java.util.Map;
@@ -9,12 +10,14 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 
-@Data
+
 public class FloodGame implements IGame {
 
-    private List<Player> players = new CopyOnWriteArrayList<>();
-    private Field field;
+    private @Getter @Setter List<Player> players = new CopyOnWriteArrayList<>();
+    private @Getter Field field;
     private @Getter Map<Player, Cell> playersStartPosition = new ConcurrentHashMap<>();
+    private @Getter int counter = 0;
+    private IFirstSearch firstSearch;
     public FloodGame(GameType type) {
 
         switch (type) {
@@ -25,13 +28,55 @@ public class FloodGame implements IGame {
             case FAST:
                 this.field = new Field(5, 5);
         }
-
+        this.firstSearch = new DepthFirstSearch(field);
     }
 
     @Override
-    public Result makeAction(Action action) {
+    public Result makeAction(Player player, Action action) {
+        switch (action.getType()){
+            case MAKE_STEP:
+                if(action.getParams().get("X1") instanceof Integer &&
+                        action.getParams().get("X2") instanceof Integer &&
+                        action.getParams().get("color") instanceof  Color) {
+
+                    if(isValidMakeStep(player, (Integer)action.getParams().get("X1"),
+                            (Integer) action.getParams().get("X1"),
+                            (Color) action.getParams().get("Color"))) {
+
+                        makeStep(player, (Integer) action.getParams().get("X1"),
+                                (Integer) action.getParams().get("X2"),
+                                (Color) action.getParams().get("Color"));
+                    }
+                }
+
+                break;
+            default:
+                return null;
+        }
         return null;
     }
 
+    @Override
+    public boolean addPlayer(Player player) {
+        return false;
+    }
+    @Override
+    public void removePlayer(Player player){
+
+    }
+
+    public void makeStep(Player player, int x, int y, Color color){
+        Cell tmpCell = new Cell(x, y);
+        tmpCell.setColor(color);
+        firstSearch.start(tmpCell);
+        counter = (counter + 1) % playersStartPosition.size();
+    }
+    public Boolean isValidMakeStep(Player player, int x, int y, Color color){
+        Cell tmpCell = playersStartPosition.get(counter);
+        return player == players.get(counter) &&
+                tmpCell.getX() == x && tmpCell.getY() == y &&
+                tmpCell.getColor() != color &&
+                field.isInternalAt(x, y);
+    }
 }
 
