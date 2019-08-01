41a75fc71edaa9be91932a9a7652375e6852d197
TimurShangareev <cho.tebe@mail.ru>
2019-08-01 04:41:23 +0300

    Added WebSocketService class to front'end

--- app/game/index.js
+++ app/game/index.js
@@ -10,3 +10,36 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+
+function WebSocketService(){
+    this.socket;
+    this.url = 'redirect:/index.html';
+    this.callbackOnOpen;
+    this.onOpen = function(callback){
+        this.addEventListener('open', (event) => {
+            callback();
+    });
+        this.callbackOnOpen = callback;
+    };
+    this.onClose = function (event, callback) {
+        if(!callback) callback = (event) => {};
+        this.socket.addEventListener('close', callback);
+    };
+    this.onMessage = function (event) {
+        this.socket.addEventListener('message', function (event) {
+            console.log('Message from server ', event.data);
+        });
+    };
+
+    this.init = function () {
+        this.socket = new WebSocket(this.url);
+        this.socket.addEventListener('open', (event)=>{
+            this.callbackOnOpen();
+    });
+    };
+}
+var socket = new WebSocketService();
+var onOpen = function() {
+    console.log("connected")
+}
+socket.onOpen(onOpen);
