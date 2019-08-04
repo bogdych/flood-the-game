d6389743e04d3655a41548211a2ae297bdeaf56d
TimurShangareev <cho.tebe@mail.ru>
2019-08-04 08:17:50 +0300

    Refactored

--- app/game/WebSocketService.js
+++ app/game/WebSocketService.js
@@ -1,23 +1,35 @@
 export function WebSocketService(){
     this.url = 'ws://localhost:8060/game';
-    this.callbackOnOpen;
+    this.callbackOnOpen = function () {   };
     this.socket = new WebSocket(this.url);
 
     this.init = function () {
-        this.socket.close();
-        this.socket = new WebSocket(this.url);
-        this.socket.onopen = (event) => { return this.callbackOnOpen();};
+        this.socket.onopen = (event) => { this.callbackOnOpen();};
     };
     this.onOpen = function(callback){
+        if(!callback) {
+            callback = () => {};
+        }
         this.callbackOnOpen = callback;
+        this.socket.onopen =  (event) => {this.callbackOnOpen()};
+
+        }
     };
-    this.onClose = function (event, callback) {
-        if(!callback) callback = (event) => {};
-        this.socket.onclose = function(event){ callback };
+    this.onClose = function (callback) {
+        if(!callback) {
+            callback = () => {};
+        }
+        this.socket.onclose = function(event){ callback() };
     };
-    this.onMessage = function (event) {
+    this.onMessage = function (callback) {
+        if(!callback) {
+            callback = () => {return "SomeMessage"};
+        }
         this.socket.onmessage = function (event) {
-            console.log('Message from server ', event.data);
+            console.log('Message from server ', callback());
         };
     };
+    this.send = function(data){
+        this.socket.send(data);
+    }
 }
