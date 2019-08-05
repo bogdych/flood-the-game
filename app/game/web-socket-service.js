596534f5f7cc001419e845f84b4465053e0b2f36
TimurShangareev <cho.tebe@mail.ru>
2019-08-05 21:02:24 +0300

    +Add send method, removed excess moments

--- app/game/web-socket-service.js
+++ app/game/web-socket-service.js
@@ -1,35 +1,28 @@
 export default class WebSocketService{
-    constructor() {
-        this.socket;
-        this.url = 'ws://localhost:8060/game';
-        this.callbackOnOpen = () => {console.log("Connected")};
-        this.callbackOnMessage = (data) => {console.log("Message from server", event.data)};
-        this.callbackOnClose = () => {console.log("Closed")};
-    }
+    static URL = "ws://localhost:8060/game";
 
+    constructor() {
+        this.callbackOnOpen = () => console.log("Connected");
+        this.callbackOnMessage = (data) => console.log("Message from server", event.data);
+        this.callbackOnClose = () => console.log("Closed");
+    };
     init() {
-        if(!this.socket){ this.socket = new WebSocket(this.url)}
-        this.socket.onopen =(event) => { this.callbackOnOpen();}
-        this.socket.onclose = (event) => { this.callbackOnClose(event.data); }
-        this.socket.onmessage = (event) => { this.callbackOnMessage(); }
+        if(!this.socket){
+            this.socket = new WebSocket(URL);
+            this.socket.onopen =(event) => this.callbackOnOpen();
+            this.socket.onclose = (event) => this.callbackOnClose(event.data);
+            this.socket.onmessage = (event) => this.callbackOnMessage();
+        }
     };
     onOpen(callback) {
-        if(!callback){
-            callback = this.callbackOnOpen; }
-        else {
-            this.callbackOnOpen = callback;}
+        this.callbackOnOpen = callback;
     };
 
     onClose(callback) {
-        if(!callback){
-            callback = this.callbackOnClose; }
-        else{
-            this.callbackOnClose = callback};
+        this.callbackOnClose = callback;
     };
     onMessage(callback) {
-        if(!callback){
-            callback = this.callbackOnMessage; }
-        else{
-            this.callbackOnMessage = callback;}
+        this.callbackOnMessage = callback;
     };
+    send(data) {this.socket.send(data);}
 }
