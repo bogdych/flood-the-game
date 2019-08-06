26784988cc6a71b7bb44ba17c98096a12c3beaf5
TimurShangareev <cho.tebe@mail.ru>
2019-08-06 21:55:36 +0300

    Test problem. Uncaught DOMException: Failed to execute 'send' on 'WebSocket': Still in CONNECTING state.

--- app/game/multi-player-service.js
+++ app/game/multi-player-service.js
@@ -5,21 +5,15 @@ export default class MultiplayerService{
     constructor(){
         this.playerData = new PlayerData();
         this.socket = new WebSocketService();
-        this.socket.onMessage((data) => this.onMessage(data));
+        this.socket.onMessage((event) => this.onMessage(event.data));
         this.socket.init();
+
     }
     //TODO: should I each case delegate a corresponding method ( gameReady() )?
     //Нужно ли вынести все case в отдельные методы?
-    //SyntaxError: Unexpected token u in JSON at position 0
-    //in try block
     onMessage(data){
-        try {
-            let msg = JSON.parse(data);
-        }catch (e) {
-            console.log(e + '');
-            return;
-        }
-        if(type in msg){
+        let msg = JSON.parse(data);
+        if("type" in msg){
             switch(msg.type){
                 case "error":
                     alert("Error");
@@ -33,9 +27,9 @@ export default class MultiplayerService{
                     break;
             }
         } else{
-            if(id in msg){
+            if("id" in msg){
                 this.id = msg.id;
-                console.log("id player set");
+                console.log("id player set" + this.id);
             } else{
                 alert("Unexpected response from server");
             }
@@ -47,8 +41,6 @@ export default class MultiplayerService{
             name: "flood",
             gameType:"standard"
         };
-        if(this.socket.socket.readyState === WebSocket.OPEN){
-            this.socket.send(JSON.stringify(msgToServer));
-        }
+        this.socket.send(JSON.stringify(msgToServer));
     }
 }
