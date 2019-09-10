6ed62a36dde52fcfc4e64da6d227e693b745d363
TimurShangareev <cho.tebe@mail.ru>
2019-09-10 11:20:00 +0300

    +Changed Logic in DepthFirstSearch

--- src/main/java/com/summerschool/flood/game/flood/DepthFirstSearch.java
+++ src/main/java/com/summerschool/flood/game/flood/DepthFirstSearch.java
@@ -19,7 +19,10 @@ public class DepthFirstSearch implements IFirstSearch {
         List<Cell> changeList= new ArrayList<>();
         startColor = field.getCells()[x][y].getColor();
         fillColor = color;
-        runNext(x, y, otherPlayers, changeList);
+        runNext(x, y);
+        Boolean[][] used = new Boolean[field.getWidth()][field.getWidth()];
+        fillBooleanArray(used, false);
+        runVoidNext(x, y,  otherPlayers, changeList, used);
         return changeList;
     }
 
@@ -28,20 +31,39 @@ public class DepthFirstSearch implements IFirstSearch {
      * @param x X coordinate of the cell to be filled
      * @param y Y coordinate of the cell to be filled
      */
-    private void runNext(int x, int y, List<Cell> otherPlayers, List<Cell> changeList) {
+    private void runNext(int x, int y) {
         if (!field.isInternalAt(x, y) ||
-            field.getCells()[x][y].getColor() != startColor)
+                field.getCells()[x][y].getColor() != startColor)
             return;
-
-        if (otherPlayers.contains(field.getCells()[x][y]))
-            changeList.add(field.getCells()[x][y]);
 
         field.getCells()[x][y].setColor(fillColor);
 
-        runNext(x - 1, y, otherPlayers, changeList);
-        runNext(x + 1, y, otherPlayers, changeList);
-        runNext(x, y - 1, otherPlayers, changeList);
-        runNext(x, y + 1, otherPlayers, changeList);
+        runNext(x - 1, y);
+        runNext(x + 1, y);
+        runNext(x, y - 1);
+        runNext(x, y + 1);
     }
 
+    private void runVoidNext(int x, int y, List<Cell> otherPlayers, List<Cell> changeList, Boolean[][] used) {
+        if (!field.isInternalAt(x, y) ||
+                field.getCells()[x][y].getColor() != fillColor ||
+                used[x][y])
+            return;
+        used[x][y] = true;
+        if (otherPlayers.contains(field.getCells()[x][y]))
+            changeList.add(field.getCells()[x][y]);
+
+
+        runVoidNext(x - 1, y, otherPlayers, changeList, used);
+        runVoidNext(x + 1, y, otherPlayers, changeList, used);
+        runVoidNext(x, y - 1, otherPlayers, changeList, used);
+        runVoidNext(x, y + 1, otherPlayers, changeList, used);
+    }
+    private void fillBooleanArray(Boolean[][] array, Boolean value) {
+        for (int x = 0; x < array.length; x++) {
+            for (int y = 0; y < array[x].length; y++) {
+                array[x][y] = value;
+            }
+        }
+    }
 }
