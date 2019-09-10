c8948d75c1687d601deed21882e42d3d15d4cfa9
nevor.nevor <decembrist.org@gmail.com>
2019-09-11 02:02:59 +0300

    FLOOD-33: minor refactor

--- src/main/java/com/summerschool/flood/game/flood/DepthFirstSearch.java
+++ src/main/java/com/summerschool/flood/game/flood/DepthFirstSearch.java
@@ -7,63 +7,68 @@ import java.util.ArrayList;
 
 public class DepthFirstSearch implements IFirstSearch {
 
-    private Color startColor;
-    private Color fillColor;
-    private Field field;
+	private Color startColor;
+	private Color fillColor;
+	private Field field;
 
-    public DepthFirstSearch(Field field){
-        this.field = field;
-    }
+	public DepthFirstSearch(Field field) {
+		this.field = field;
+	}
 
-    public List<Cell> start(int x, int y, Color color, List<Cell> otherPlayers) {
-        List<Cell> changeList= new ArrayList<>();
-        startColor = field.getCells()[x][y].getColor();
-        fillColor = color;
-        runNext(x, y);
-        Boolean[][] used = new Boolean[field.getWidth()][field.getWidth()];
-        fillBooleanArray(used, false);
-        runVoidNext(x, y,  otherPlayers, changeList, used);
-        return changeList;
-    }
+	public List<Cell> start(int x, int y, Color color, List<Cell> otherPlayers) {
+		List<Cell> changeList = new ArrayList<>();
+		startColor = field.getCells()[x][y].getColor();
+		fillColor = color;
+		runNext(x, y);
+		boolean[][] used = new boolean[field.getWidth()][field.getWidth()];
+		runVoidNext(x, y, otherPlayers, changeList, used);
+		return changeList;
+	}
 
-    /**
-     * Recursive fill of the game field with chosen color from specified point
-     * @param x X coordinate of the cell to be filled
-     * @param y Y coordinate of the cell to be filled
-     */
-    private void runNext(int x, int y) {
-        if (!field.isInternalAt(x, y) ||
-                field.getCells()[x][y].getColor() != startColor)
-            return;
+	/**
+	 * Recursive fill of the game field with chosen color from specified point
+	 *
+	 * @param x X coordinate of the cell to be filled
+	 * @param y Y coordinate of the cell to be filled
+	 */
+	private void runNext(int x, int y) {
+		if (!field.isInternalAt(x, y) || field.getCells()[x][y].getColor() != startColor) {
+			return;
+		}
 
-        field.getCells()[x][y].setColor(fillColor);
+		final Cell cell = field.getCells()[x][y];
+		cell.setColor(fillColor);
 
-        runNext(x - 1, y);
-        runNext(x + 1, y);
-        runNext(x, y - 1);
-        runNext(x, y + 1);
-    }
+		runNext(x - 1, y);
+		runNext(x + 1, y);
+		runNext(x, y - 1);
+		runNext(x, y + 1);
+	}
 
-    private void runVoidNext(int x, int y, List<Cell> otherPlayers, List<Cell> changeList, Boolean[][] used) {
-        if (!field.isInternalAt(x, y) ||
-                field.getCells()[x][y].getColor() != fillColor ||
-                used[x][y])
-            return;
-        used[x][y] = true;
-        if (otherPlayers.contains(field.getCells()[x][y]))
-            changeList.add(field.getCells()[x][y]);
+	/**
+	 * TODO Комментарий
+	 *
+	 * @param x
+	 * @param y
+	 * @param otherPlayers
+	 * @param changeList
+	 * @param used
+	 */
+	private void runVoidNext(int x, int y, List<Cell> otherPlayers, List<Cell> changeList, boolean[][] used) {
+		if (!field.isInternalAt(x, y)
+				|| field.getCells()[x][y].getColor() != fillColor
+				|| used[x][y]) {
+			return;
+		}
+		used[x][y] = true;
+		final Cell cell = field.getCells()[x][y];
+		if (otherPlayers.contains(cell)) {
+			changeList.add(cell);
+		}
 
-
-        runVoidNext(x - 1, y, otherPlayers, changeList, used);
-        runVoidNext(x + 1, y, otherPlayers, changeList, used);
-        runVoidNext(x, y - 1, otherPlayers, changeList, used);
-        runVoidNext(x, y + 1, otherPlayers, changeList, used);
-    }
-    private void fillBooleanArray(Boolean[][] array, Boolean value) {
-        for (int x = 0; x < array.length; x++) {
-            for (int y = 0; y < array[x].length; y++) {
-                array[x][y] = value;
-            }
-        }
-    }
+		runVoidNext(x - 1, y, otherPlayers, changeList, used);
+		runVoidNext(x + 1, y, otherPlayers, changeList, used);
+		runVoidNext(x, y - 1, otherPlayers, changeList, used);
+		runVoidNext(x, y + 1, otherPlayers, changeList, used);
+	}
 }
