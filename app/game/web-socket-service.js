f358ae2c90e09fcada31ee90b13b5846b183b0ae
nevor.nevor <decembrist.org@gmail.com>
2019-09-15 00:07:32 +0300

    add dynamic web-socket address

--- app/game/web-socket-service.js
+++ app/game/web-socket-service.js
@@ -1,29 +1,41 @@
-export default class WebSocketService{
-    static get URL() { return "ws://localhost:8060/game";}
+export default class WebSocketService {
+	static get URL() {
+		let port = window.location.port;
+		if (!port) {
+			port = window.location.protocol === "http" ? 80 : 443;
+		}
+		return `ws://${window.location.hostname}:${port}/game`;
+	}
 
-    constructor() {
-        this.callbackOnOpen = () => console.log("Connected");
-        this.callbackOnMessage = (event) => console.log("Message from server", event.data);
-        this.callbackOnClose = () => console.log("Closed");
-    };
-    init() {
-        if(!this.socket){
-            this.socket = new WebSocket(WebSocketService.URL);
-            this.socket.onopen =(event) => this.callbackOnOpen();
-            this.socket.onclose = (event) => this.callbackOnClose(event.data);
-            this.socket.onmessage = (event) => this.callbackOnMessage(event);
-        }
-    };
-    onOpen(callback) {
-        this.callbackOnOpen = callback;
-    };
+	constructor() {
+		this.callbackOnOpen = () => console.log("Connected");
+		this.callbackOnMessage = (event) => console.log("Message from server", event.data);
+		this.callbackOnClose = () => console.log("Closed");
+	};
 
-    onClose(callback) {
-        this.callbackOnClose = callback;
-    };
-    onMessage(callback) {
-        this.callbackOnMessage = callback;
-    };
-    send(data) {this.socket.send(data);}
+	init() {
+		if (!this.socket) {
+			this.socket = new WebSocket(WebSocketService.URL);
+			this.socket.onopen = (event) => this.callbackOnOpen();
+			this.socket.onclose = (event) => this.callbackOnClose(event.data);
+			this.socket.onmessage = (event) => this.callbackOnMessage(event);
+		}
+	};
+
+	onOpen(callback) {
+		this.callbackOnOpen = callback;
+	};
+
+	onClose(callback) {
+		this.callbackOnClose = callback;
+	};
+
+	onMessage(callback) {
+		this.callbackOnMessage = callback;
+	};
+
+	send(data) {
+		this.socket.send(data);
+	}
 
 }
